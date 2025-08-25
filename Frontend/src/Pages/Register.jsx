24e3930829dd7cd6f12e789c4eeb3ca175f632b8
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-08-26 02:07:25 +0530

    ..

--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { School, Mail, Key, User, GraduationCap,Calendar } from "lucide-react";
 import { Link } from "react-router-dom";
 
