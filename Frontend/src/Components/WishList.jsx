8bf7494fe3c01a7e365a1a5412bfe55fd4bbe589
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-09-09 21:17:11 +0530

    wishlist implemented

--- Frontend/src/Components/WishList.jsx
+++ Frontend/src/Components/WishList.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import img from "../assets/img.jpeg";
+import {Trash} from "lucide-react";
 
-const ListingListCard = () => {
+const WishList = () => {
   const [listing, setListing] = useState({
   "id": "c9d8e7f6-a5b4-3210-fedc-0987654321ab",
   "type": "SERVICE",
-  "title": "Math Tutoring - Calculus & Algebra",
+  "title": "Math  TutoriCalculus caulus  Tutoring - Calculus & Algeb- Calculus & Algebra M  & Algebra M",
   "price": 500.00,
   "description": "Experienced tutor offering 1-on-1 sessions for Calculus, Algebra, and Engineering Math. Available evenings and weekends.",
   "institute": "Delhi University",
@@ -43,8 +44,8 @@ const ListingListCard = () => {
 </div> 
 
       {/* Product detail section */}
-      <div className="flex flex-col gap-1 pr-2 flex-grow w-[60vw]  ">
-        <h2 className="text-base sm:text-lg md:text-xl font-medium text-gray-600 overflow-x-auto whitespace-nowrap">
+      <div className="flex flex-col gap-1 pr-2 flex-grow w-full  ">
+        <h2 className="text-base sm:text-lg md:text-xl font-medium text-gray-600 line-clamp-1  w-full">
           {listing.title}
         </h2>
         <div className="flex justify-between items-center">
@@ -53,21 +54,20 @@ const ListingListCard = () => {
           </div>
           
         </div>
-        <div className="  overflow-x-auto text-slate-500 whitespace-nowrap " >
-            {listing.description}
-        </div>
+        
         <div className="flex justify-between pt-1 ">
-          <div className="font-light text-slate-500 text-sm text-right " >
+          <div className="font-light text-slate-500 text-sm text-right  " >
             {new Date(listing.createdAt).toLocaleDateString("en-GB", {
               day: "numeric",
               month: "long",
             })}
           </div>
-          <div className=" text-xs font-medium bg-indigo-100 text-indigo-700 px-1 py-0.5 rounded-sm  ">{listing.type}</div>
+          <div className=" text-xs font-medium  text-indigo-700 px-1 py-0.5 rounded-sm hover:text-red-700 hover:fill-red-600"><Trash />
+</div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ListingListCard;
+export default WishList;
--- Frontend/src/Routes/MyRoute.jsx
+++ Frontend/src/Routes/MyRoute.jsx
@@ -10,6 +10,7 @@ import ListingListCard from "../Components/ListingListCard";
 import ListingCard from "../Components/ListingCard";
 import Home from "../Pages/Home";
 import Categories from "../Components/Categories";
+import WishList from "../Components/WishList";
 
 const MyRoute = createBrowserRouter(
     createRoutesFromElements(
@@ -17,9 +18,10 @@ const MyRoute = createBrowserRouter(
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
-      <Route path="/list" element={<ListingListCard/>} />
-      <Route path="/list-card" element={<ListingCard/>} />
+      <Route path="/list" element={<ListingListCard/>}/>
+      <Route path="/list-card" element={<ListingCard/>}/>
       <Route path="/cat" element={<Categories/>} />
+      <Route path="/wishlist" element={<WishList/>}/>
       
  
       </>
