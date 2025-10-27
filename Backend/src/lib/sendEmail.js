fc4d846b9ee73f92ce2d7fd3052c580ae5df4423
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-10-27 17:38:40 +0530

    changes in sendemail

--- Backend/src/lib/sendEmail.js
+++ Backend/src/lib/sendEmail.js
@@ -3,12 +3,12 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ to, subject, text, html }) => {
   try {
 
-console.log('--- Initializing Nodemailer ---');
-console.log('EMAIL_HOST from env:', process.env.SMTP_HOST);
-console.log('EMAIL_PORT from env:', process.env.SMTP_PORT);
-console.log('SMTP_USER from env:', process.env.SMTP_USER);
-console.log('SMTP_PASS from env:', process.env.SMTP_PASS);
-console.log('-----------------------------');
+// console.log('--- Initializing Nodemailer ---');
+// console.log('EMAIL_HOST from env:', process.env.SMTP_HOST);
+// console.log('EMAIL_PORT from env:', process.env.SMTP_PORT);
+// console.log('SMTP_USER from env:', process.env.SMTP_USER);
+// console.log('SMTP_PASS from env:', process.env.SMTP_PASS);
+// console.log('-----------------------------');
     
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,      // e.g., smtp.gmail.com
@@ -36,3 +36,31 @@ console.log('-----------------------------');
     return false;
   }
 };
+
+
+// import { Resend } from "resend";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const sendEmail = async ({ to, subject, text, html }) => {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: "CampusKart <onboarding@resend.dev>",
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+
+//     if (error) {
+//       console.error("❌ Failed to send email:", error);
+//       return false;
+//     }
+
+//     console.log("✅ Email sent successfully:", data?.id || "(no id returned)");
+//     return true;
+//   } catch (err) {
+//     console.error("❌ Error sending email:", err);
+//     return false;
+//   }
+// };
