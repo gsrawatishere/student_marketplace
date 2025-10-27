ff717e219e13443e76a5e96b4d1286931a7a1583
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-10-27 23:35:07 +0530

    changes in index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -20,8 +20,7 @@ const corsOptions = {
   credentials: true, 
   optionsSuccessStatus: 200 
 };
-app.use(cors(corsOptions));
-app.use(express.json()); 
+app.use(cors(corsOptions)); 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
